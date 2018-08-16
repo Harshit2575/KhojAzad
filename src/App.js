c06f25bbe7353e2b621eb7281a3c68c197f1d232
RishitSinha <rishitsinha25@gmail.com>
2018-08-16 19:20:07 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React, {Component} from 'react';
+import './App.css';
+import data from './data2';
+
+let md5 = require('md5');
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [
+                {
+                    question: `1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum exercitationem explicabo laboriosam libero neque nisi officiis quibusdam vel veniam.`,
+                    answer: `Lorem`,
+                },
+                {
+                    question: `2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum exercitationem explicabo laboriosam libero neque nisi officiis quibusdam vel veniam.`,
+                    answer: `Lorem`,
+                },
+                {
+                    question: `3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum exercitationem explicabo laboriosam libero neque nisi officiis quibusdam vel veniam.`,
+                    answer: `Lorem`,
+                },
+                {
+                    question: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum exercitationem explicabo laboriosam libero neque nisi officiis quibusdam vel veniam.`,
+                    answer: `Lorem`,
+                },
+                {
+                    question: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum exercitationem explicabo laboriosam libero neque nisi officiis quibusdam vel veniam.`,
+                    answer: `Lorem`,
+                },
+                {
+                    question: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum exercitationem explicabo laboriosam libero neque nisi officiis quibusdam vel veniam.`,
+                    answer: `Lorem`,
+                },
+                {
+                    question: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cum exercitationem explicabo laboriosam libero neque nisi officiis quibusdam vel veniam.`,
+                    answer: `Lorem`,
+                },
+            ],
+            progress: [],
+            questions: [],
+            selectedTeam: 0,
+            tempTeam: 1,
+            answer: '',
+            totalTeams: 30,
+        }
+    }
+
+    componentDidMount() {
+        const saveData = window.localStorage.getItem('progress');
+        if (saveData) {
+            const data = JSON.parse(saveData);
+            this.setState({progress: data.progress, questions: data.questions});
+        }
+        else {
+            let {totalTeams} = this.state;
+            const last = data.splice(data.length - 1);
+            const rest = data.splice(1);
+            const first = data[0];
+            this.setState({
+                progress: Array.from({length: totalTeams}).map(() => 0),
+                questions: Array.from({length: totalTeams}).map(() => [first, ...(this.shuffle(rest)), last])
+            });
+        }
+    }
+
+    render() {
+        const {questions, progress, selectedTeam, tempTeam, answer} = this.state;
+        return (
+            <div className="App">
+                <h1>Team Number</h1>
+                <form onSubmit={this.updateTeamIndex}>
+                    <input
+                        onChange={e => this.setState({tempTeam: e.target.value})}
+                        value={tempTeam}
+                        type="number"/>
+                </form>
+
+                {
+                    progress.length && questions.length &&
+                    <h1>{progress[selectedTeam] + 1}. {questions[selectedTeam][progress[selectedTeam]].question}</h1>
+                }
+                {
+                    progress.length && questions.length && progress[selectedTeam] !== questions[selectedTeam].length - 1
+                        ? <form onSubmit={this.submit}>
+                            <input
+                                type="text"
+                                onChange={e => this.setState({answer: e.target.value})}
+                                value={answer}/>
+                        </form>
+                        : null
+                }
+            </div>
+        );
+    }
+
+    submit = e => {
+        e.preventDefault();
+        const {answer, questions, progress, selectedTeam} = this.state;
+        if (md5(answer.toLowerCase().split(' ').join('').replace(/[^\w\s]/gi, '')).toLowerCase() === questions[selectedTeam][progress[selectedTeam]].answer.toLowerCase()) {
+            progress[selectedTeam] = progress[selectedTeam] + 1;
+            this.setState({
+                progress,
+                answer: ''
+            });
+        }
+        else {
+            alert('Wrong answer.')
+        }
+        const saveData = JSON.stringify({
+            progress,
+            questions
+        });
+        window.localStorage.setItem('progress', saveData);
+    };
+
+    updateTeamIndex = e => {
+        e.preventDefault();
+        const {tempTeam, totalTeams} = this.state;
+        let teamIndex = tempTeam < 1
+            ? 1
+            : tempTeam > totalTeams
+                ? totalTeams
+                : tempTeam;
+        this.setState({
+            tempTeam: teamIndex,
+            selectedTeam: teamIndex - 1
+        })
+    };
+
+    shuffle = array => {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+}
+
+export default App;
